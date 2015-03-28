495d20b9c049f158a793ffb8668e9817687ddfaa
cesaenzv <cesaenzv@hotmail.com>
2015-03-28 09:29:41 -0500

    Initial Config
    Configuración inicial del ambiente

---
+++ tests/DataBaseTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* 
+*/
+class DataBaseTest extends TestCase
+{
+	
+
+	public function testConnection(){
+
+		$dbName = DB::connection()->getDatabaseName();
+		$this->assertEquals("gravility", $dbName);
+	}
+}
